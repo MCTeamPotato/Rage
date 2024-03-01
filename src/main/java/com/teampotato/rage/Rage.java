2e2177df1911d0a74bc4558a11f9cee28742db11
Kasualix <670067575@qq.com>
2024-03-01 13:32:18 +0800

    0.0.2

--- src/main/java/com/teampotato/rage/Rage.java
+++ src/main/java/com/teampotato/rage/Rage.java
@@ -2,7 +2,9 @@ package com.teampotato.rage;
 
 import com.teampotato.rage.api.RageHolder;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -10,6 +12,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -25,13 +28,14 @@ public class Rage {
         IEventBus forgeBus = MinecraftForge.EVENT_BUS;
         forgeBus.addListener(this::bumpOrUseRageOnAttack);
         forgeBus.addListener(this::bumpRageOnHurt);
-        forgeBus.addListener(this::showParticleOnFullRage);
+        forgeBus.addListener(this::showParticleOnFullRageLiving);
+        forgeBus.addListener(this::showParticleOnFullRagePlayer);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CONFIG);
     }
 
     public static final ForgeConfigSpec CONFIG;
-    public static final ForgeConfigSpec.BooleanValue NOTIFY_PLAYER_ON_RAGE_CHANGE, SHOW_PARTICLE_ON_FULL_RAGE;
-    public static final ForgeConfigSpec.ConfigValue<Double> MAX_DAMAGE_BONUS, BASIC_DAMAGE_BONUS;
+    public static final ForgeConfigSpec.BooleanValue NOTIFY_PLAYER_ON_RAGE_CHANGE, SHOW_PARTICLE_ON_FULL_RAGE, PLAY_DING_ON_FULL_RAGE_ATTACK;
+    public static final ForgeConfigSpec.DoubleValue MAX_DAMAGE_BONUS, BASIC_DAMAGE_BONUS, DING_VOLUME, DING_PITCH;
     public static final ForgeConfigSpec.IntValue FULL_RAGE_VALUE, GAINED_RAGE_PER_HURT_OR_ATTACK;
 
     static {
@@ -44,15 +48,28 @@ public class Rage {
         FULL_RAGE_VALUE = builder.defineInRange("FullRageValue", 150, 0, Integer.MAX_VALUE);
         GAINED_RAGE_PER_HURT_OR_ATTACK = builder.defineInRange("GainedRagePerHurtOrAttack", 50, 0, Integer.MAX_VALUE);
         builder.pop();
+        builder.push("Sound");
+        PLAY_DING_ON_FULL_RAGE_ATTACK = builder.define("PlayDingOnFullRageAttack", true);
+        DING_VOLUME = builder.defineInRange("DingVolume", 1.00, 0.00, Double.MAX_VALUE);
+        DING_PITCH = builder.defineInRange("DingPitch", 1.00, 0.00, Double.MAX_VALUE);
+        builder.pop();
         CONFIG = builder.build();
     }
 
-    private void showParticleOnFullRage(TickEvent.PlayerTickEvent event) {
+    private void showParticleOnFullRagePlayer(TickEvent.PlayerTickEvent event) {
         if (!SHOW_PARTICLE_ON_FULL_RAGE.get()) return;
         if (event.phase != TickEvent.Phase.START) return;
         Player player = event.player;
         if (((RageHolder)player).rage$isFullRage() && player instanceof ServerPlayer) {
             ((ServerPlayer) player).getLevel().sendParticles(ParticleTypes.CRIT, player.getX(), player.getY(), player.getZ(), 8, 0.2, 0.2, 0.2, 0.0);
+        }
+    }
+
+    private void showParticleOnFullRageLiving(LivingEvent.LivingUpdateEvent event) {
+        if (!SHOW_PARTICLE_ON_FULL_RAGE.get()) return;
+        LivingEntity entity = event.getEntityLiving();
+        if (((RageHolder)entity).rage$isFullRage() && entity.level instanceof ServerLevel) {
+            ((ServerLevel) entity.level).sendParticles(ParticleTypes.CRIT, entity.getX(), entity.getY(), entity.getZ(), 8, 0.2, 0.2, 0.2, 0.0);
         }
     }
 
@@ -82,6 +99,7 @@ public class Rage {
         if (entity instanceof LivingEntity) {
             if (((RageHolder)entity).rage$isFullRage()) {
                 event.setAmount((float) (((RageHolder)entity).rage$getDamageBonus() * (double) event.getAmount()));
+                if (PLAY_DING_ON_FULL_RAGE_ATTACK.get()) entity.playSound(SoundEvents.ARROW_HIT_PLAYER, DING_VOLUME.get().floatValue(), DING_PITCH.get().floatValue());
                 ((RageHolder) entity).rage$clearRage();
             } else {
                 ((RageHolder)entity).rage$bumpRage();
