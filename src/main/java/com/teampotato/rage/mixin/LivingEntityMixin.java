abf1717606662a61b5c926257591491b5187041f
Kasualix <670067575@qq.com>
2024-03-02 15:29:51 +0800

    Update LivingEntityMixin.java

--- src/main/java/com/teampotato/rage/mixin/LivingEntityMixin.java
+++ src/main/java/com/teampotato/rage/mixin/LivingEntityMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import net.minecraftforge.server.ServerLifecycleHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
